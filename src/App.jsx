00d9c0a2717dcd88cd4c85ddfde7fdd0847f728f
i-nirbhay10 <nitbhayverma10@gmail,com>
2025-05-10 12:32:42 +0530

    bag fix

--- src/App.jsx
+++ src/App.jsx
@@ -106,13 +106,13 @@ function App() {
         {/* Scrollable Chat Container - Updated className */}
         <div
           ref={chatContainerRef}
-          className="flex-1 overflow-x-auto w-full mb-4 rounded-lg bg-[#1F2123] shadow-lg p-4 hide-scrollbar "
+          className="flex-1 overflow-auto w-full mb-4 rounded-lg bg-[#1F2123] shadow-lg md:p-4 hide-scrollbar"
         >
           {chatHistory.length === 0 ? (
             <>
               {checked ? (
                 <div className="h-full flex flex-col items-center justify-center text-center p-6">
-                  <div className="rounded-xl md:p-8 max-w-2xl">
+                  <div className="rounded-xl md:p-8 max-w-2xl py-6">
                     <h2 className="text-xl md:text-2xl font-bold text-[#8E7F85]   mb-4">
                       Welcome to ChatBot AI! 👋
                     </h2>
@@ -180,7 +180,7 @@ function App() {
                 </div>
               ) : (
                 <div className="h-full flex flex-col items-center justify-center text-center p-6">
-                  <div className="rounded-xl md:p-8 max-w-2xl">
+                  <div className="rounded-xl md:p-8 max-w-2xl py-6">
                     <h2 className="text-xl md:text-2xl font-bold text-[#8E7F85]   mb-4">
                       Welcome to Nirbhay's Portfolio Chatbot! 👋
                     </h2>
@@ -306,6 +306,28 @@ function App() {
               }}
             />
 
+            <button
+              type="submit"
+              className={`flex items-center gap-3 px-2 md:px-6 py-2 bg-gradient-to-r from-[#1F2123] to-red-400 text-white rounded-md hover:bg-blue-600 transition-colors ${
+                generatingAnswer ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              disabled={generatingAnswer}
+            >
+              <span className=" hidden md:block ">Ask</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+
             {/* <textarea
               required
               className="flex-1 border border-gray-300 rounded p-3 focus:border-blue-400 focus:ring-1 focus:ring-blue-400 resize-none"
@@ -320,27 +342,6 @@ function App() {
                 }
               }}
             ></textarea> */}
-            <button
-              type="submit"
-              className={`flex items-center gap-3 px-6 py-2 bg-gradient-to-r from-[#1F2123] to-red-400 text-white rounded-md hover:bg-blue-600 transition-colors ${
-                generatingAnswer ? "opacity-50 cursor-not-allowed" : ""
-              }`}
-              disabled={generatingAnswer}
-            >
-              Ask
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
           </div>
         </form>
       </div>
